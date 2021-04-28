a0860e4df0352259661390c86f7145b7de5b98c4
Hamish Gow <hamishgow@hamishs-air.lan>
2021-04-28 13:49:37 +1000

    modify src/App.js text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sixth Edit.
+          New Text - updated when on branch: Feature-01
         </p>
         <a
           className="App-link"
